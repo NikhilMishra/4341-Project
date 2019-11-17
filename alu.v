d80cfaad7341050c2e51f83c1a276145801389f3
Max Loeffler <max_loeffler@eogresources.com>
2019-11-17 23:48:05 +0000

    somewhat integrated add function

--- alu.v
+++ alu.v
@@ -1,5 +1,38 @@
 //compiled with icarus iverilog v10.1 stable, downloaded from ubuntu apt-get
 
+module FA(a,b,c,s,co);
+input [7:0]a,b;
+output [8:0]s;
+output co;
+input c;
+Carry c1(a[0],b[0],c,s[0],cx0);
+Carry c2(a[1],b[1],cx0,s[1],cx1);
+Carry c3(a[2],b[2],cx1,s[2],cx2);
+Carry c4(a[3],b[3],cx2,s[3],cx3);
+Carry c5(a[4],b[4],cx3,s[4],cx4);
+Carry c6(a[5],b[5],cx4,s[5],cx5);
+Carry c7(a[6],b[6],cx5,s[6],cx6);
+Carry c8(a[7],b[7],cx6,s[7],s[8]);
+assign co = s[8];
+endmodule
+
+
+module Carry(a,b,c,s,co);
+input a,b;
+input c;
+output s,co;
+wire out4;
+wire out6;
+and (out1,a,b);
+xor (out2,a,b);
+and (out3,out2,c);
+or (out4,out3,out1);
+assign co = out4;
+xor (out5,a,b);
+xor (out6,out5,c);
+assign s = out6;
+endmodule
+
 module Alu_int (input [15:0] a, b, input [1:0] op, output [31:0] res, output err);
 endmodule
 
@@ -72,14 +105,14 @@ module testbench();
   $display ("A               |B               |M|Op|Result                          |E");
   $display ("================+================+=+==+================================+=");
   
-  for(mode=0; mode<1; mode=mode+1)
+  for(mode=0; mode<2; mode=mode+1)
   begin
     for(op=0; op<4; op=op+1)
     begin
-      for (a = 0; a < 16; a = a + 1) 
+      for (a = 0; a < 65536; a = a + 1) 
       begin
         #5
-        for (b = 0; b < 16; b = b + 1) 
+        for (b = 0; b < 65536; b = b + 1) 
         begin
           #5
           $display ("%16d|%16d|%d|%2d|%32d|%d",a,b,mode,op,res,err);
